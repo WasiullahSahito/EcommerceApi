acddac4a4b6dd64f88b5cc8e271f2167bec958e2
WasiullahSahito <wasi1237585@gmail.com>
2025-08-27 18:12:17 +0500

    Database Seeder Set

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProductSeeder::class,
+            OrderSeeder::class,
+            CartSeeder::class,
         ]);
     }
 }
