d7cf1cbd655df1203e350754a0ea6e5302455455
WasiullahSahito <wasi1237585@gmail.com>
2025-08-27 12:44:27 +0500

    Product Factory Added

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'name' => $this->faker->word(),
+            'description' => $this->faker->sentence(),
+            'price' => $this->faker->randomFloat(2, 50, 2000), // 50–2000
+            'stock' => $this->faker->numberBetween(1, 100),
+            'image' => $this->faker->imageUrl(640, 480, 'products', true), // fake image
+            'is_active' => true,
+        ];
+    }
+}
